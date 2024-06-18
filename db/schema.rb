a77468bf25bb801540289ef4c17c308fd9626eb1
Sean McEvoy <sfmcevoy@gmail.com>
2024-06-18 07:05:42 -0500

    Add crawl requests
    Adds a model for crawl requests, which will be used to queue up URLs to
    be crawled.
    
    As of now, I'm thinking the user will eventually be able to submit a
    URL to be crawled, and the system will queue up the URL to be crawled
    by the crawler. The crawler will then fetch the page, parse it, and
    store the response in S3. The metadata about the crawl response will be
    stored in the database. But all this is still TBD.

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 0) do
+ActiveRecord::Schema[7.2].define(version: 2024_06_18_111930) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "crawl_requests", force: :cascade do |t|
+    t.string "url", null: false
+    t.integer "crawl_type", null: false
+    t.integer "status", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
