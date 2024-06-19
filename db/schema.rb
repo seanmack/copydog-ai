d1fd755755209b831697bfdcdddaf18a19f70577
Sean McEvoy <sfmcevoy@gmail.com>
2024-06-19 07:11:29 -0500

    Add failure_message column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_06_18_111930) do
+ActiveRecord::Schema[7.2].define(version: 2024_06_19_103621) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,5 +20,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_06_18_111930) do
     t.integer "status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "failure_message"
   end
 end
