0636c0023a0a940342a3def666a6e39ae0c5cc9d
Sean McEvoy <sfmcevoy@gmail.com>
2024-06-20 05:46:58 -0500

    Crawls save HTML response (#6)
    ### Changes
    
    The HTML response retrieved during a crawl is now saved as an
    attachment.
    
    ### Why?
    
    We want to persist the HTML response so we can analyze and modify it.
    We'll eventually store these attachments on S3 or similar, but for now
    storing them locally works well.
    
    ### Screenshots
    
    ![Screenshot 2024-06-20 at 5 31
    51 AM](https://github.com/seanmack/copydog-ai/assets/5815877/18d2f8b1-98a0-497e-a7f3-37f3e5cd8502)
    ![Screenshot 2024-06-20 at 5 32
    18 AM](https://github.com/seanmack/copydog-ai/assets/5815877/8b9d4089-eaff-4ecb-ae85-2578d41af738)

--- app/jobs/crawl_web_page_job.rb
+++ app/jobs/crawl_web_page_job.rb
@@ -6,10 +6,27 @@ class CrawlWebPageJob < ApplicationJob
     result = crawler.fetch(crawl_request.url)
 
     if result[:success]
-      # TODO: Save the response and details
-      crawl_request.update(status: "completed", failure_message: nil)
+      handle_success(crawl_request, result)
     else
-      crawl_request.update(status: "failed", failure_message: result[:error])
+      handle_failure(crawl_request, result)
     end
+  end
+
+  private
+
+  def handle_success(crawl_request, result)
+    crawl_request.html_response.purge if crawl_request.html_response.attached?
+
+    crawl_request.html_response.attach(
+      io: StringIO.new(result[:body]),
+      filename: "crawl_response_#{crawl_request.id}.html",
+      content_type: "text/html"
+    )
+
+    crawl_request.update(status: "completed", failure_message: nil)
+  end
+
+  def handle_failure(crawl_request, result)
+    crawl_request.update(status: "failed", failure_message: result[:error])
   end
 end
--- spec/jobs/crawl_web_page_job_spec.rb
+++ spec/jobs/crawl_web_page_job_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe CrawlWebPageJob, type: :job do
-  it "updates the status to completed when the crawl is successful" do
+  it "updates the status to completed when the crawl is successful and attaches the HTML response" do
     crawl_request = create(:crawl_request)
     crawler = mock_crawler_with(success: true, body: "<html></html>")
 
@@ -10,6 +10,10 @@ RSpec.describe CrawlWebPageJob, type: :job do
     crawl_request.reload
     expect(crawl_request.status).to eq("completed")
     expect(crawl_request.failure_message).to be_nil
+    expect(crawl_request.html_response.attached?).to be true
+
+    html_response = crawl_request.html_response.download
+    expect(html_response).to eq("<html></html>")
   end
 
   it "updates the status to failed with the failure message when the crawl fails" do
@@ -21,6 +25,25 @@ RSpec.describe CrawlWebPageJob, type: :job do
     crawl_request.reload
     expect(crawl_request.status).to eq("failed")
     expect(crawl_request.failure_message).to eq("500")
+    expect(crawl_request.html_response.attached?).to be false
+  end
+
+  it "purges the existing attachment before attaching a new one" do
+    crawl_request = create(:crawl_request)
+    first_crawler = mock_crawler_with(success: true, body: "<html>First</html>")
+    second_crawler = mock_crawler_with(success: true, body: "<html>Second</html>")
+
+    # First time
+    described_class.perform_now(crawl_request.id, first_crawler)
+    crawl_request.reload
+    first_attachment = crawl_request.html_response.download
+    expect(first_attachment).to eq("<html>First</html>")
+
+    # Second time
+    described_class.perform_now(crawl_request.id, second_crawler)
+    crawl_request.reload
+    second_attachment = crawl_request.html_response.download
+    expect(second_attachment).to eq("<html>Second</html>")
   end
 
   def mock_crawler_with(result)
