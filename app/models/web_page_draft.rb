56d5e49ec11a1f7d972fcc6a67b2d9a212a24fa8
Sean McEvoy <sfmcevoy@gmail.com>
2024-06-25 06:30:24 -0500

    Make sure the body attribute always contains valid JSON

--- app/models/web_page_draft.rb
+++ app/models/web_page_draft.rb
@@ -3,4 +3,13 @@ class WebPageDraft < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true
+  validate :body_must_be_valid_json
+
+  private
+
+  def body_must_be_valid_json
+    JSON.parse(body) if body.present?
+  rescue JSON::ParserError
+    errors.add(:body, "must be valid JSON")
+  end
 end
--- spec/factories/web_page_drafts.rb
+++ spec/factories/web_page_drafts.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :web_page_draft do
     title { "MyString" }
-    body { "" }
+    body { '{"content": [{"title": "Section 1", "html": "<p>Content</p>"}]}' }
     crawl_request { nil }
   end
 end
